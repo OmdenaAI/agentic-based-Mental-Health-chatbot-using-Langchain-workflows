f4ad96fa62829c193520288f0f73e98c68d4e018
muhammadyahiya <yahaysfield@gmail.com>
2025-04-29 23:18:29 +0530

    Updated Working code with Project Folder Structure

---
+++ test_project/src/test_project/crew.py
@@ -0,0 +1,98 @@
+from crewai import Agent, Crew, Process, Task
+from crewai.project import CrewBase, agent, crew, task
+from crewai.knowledge.source.pdf_knowledge_source import PDFKnowledgeSource
+from crewai.agents.agent_builder.base_agent import BaseAgent
+from typing import List
+# If you want to run a snippet of code before or after the crew starts,
+# you can use the @before_kickoff and @after_kickoff decorators
+# https://docs.crewai.com/concepts/crews#example-crew-class-with-decorators
+
+
+# Create a PDF knowledge source
+pdf_source = PDFKnowledgeSource(file_paths=["literature-review-stress-anxiety-burnout-and-depression-impact-on-teachers-and-on-learner-outcomes.pdf",
+                                            "manage_stress_workbook.pdf",])
+
+memory_config = {
+  "provider": "mem0",
+	 "config": {"user_id": "User"},
+}
+
+
+@CrewBase
+class TestProject():
+    """TestProject crew"""
+
+    agents: List[BaseAgent]
+    tasks: List[Task]
+
+    # Learn more about YAML configuration files here:
+    # Agents: https://docs.crewai.com/concepts/agents#yaml-configuration-recommended
+    # Tasks: https://docs.crewai.com/concepts/tasks#yaml-configuration-recommended
+    
+    # If you would like to add tools to your agents, you can learn more about it here:
+    # https://docs.crewai.com/concepts/agents#agent-tools
+    @agent
+    def assistant(self) -> Agent:
+        return Agent(
+            config=self.agents_config['assistant'],
+            memory=True,
+            memory_config=memory_config,
+            verbose=False,
+
+        )
+        
+    @agent
+    def summarizer(self) -> Agent:
+        return Agent(
+            config=self.agents_config['summarizer'],
+            memory=True,
+            memory_config=memory_config,
+            verbose=True,
+   
+        )
+    
+    @agent  
+    def expert(self) -> Agent:
+        return Agent(
+            config=self.agents_config['expert'],
+            memory=True,
+            memory_config=memory_config,
+            verbose=False,
+        )
+
+    
+    # To learn more about structured task outputs,
+    # task dependencies, and task callbacks, check out the documentation:
+    # https://docs.crewai.com/concepts/tasks#overview-of-a-task
+    @task
+    def chat_task(self) -> Task:
+        return Task(
+            config=self.tasks_config["chat_task"],
+        )
+    
+    @task
+    def summarizer_task(self) -> Task:
+        return Task(
+            config=self.tasks_config["summarizer_task"],
+        )
+        
+    @task
+    def expert_task(self) -> Task:
+        return Task(
+            config=self.tasks_config["expert_task"],
+        )
+
+    @crew
+    def crew(self) -> Crew:
+        """Creates the CrewaiKnowledgeChatbot crew"""
+        # To learn how to add knowledge sources to your crew, check out the documentation:
+        # https://docs.crewai.com/concepts/knowledge#what-is-knowledge
+
+        return Crew(
+            agents=self.agents, # Automatically created by the @agent decorator
+            tasks=self.tasks, # Automatically created by the @task decorator
+            process=Process.sequential,
+            knowledge_sources=[pdf_source],
+            verbose=False,
+            # process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
+        )
